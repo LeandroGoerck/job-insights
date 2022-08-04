69244655e4f950d4931f85a80dbccbdb417221f3
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-08-04 17:44:40 -0300

    feat: add get_unique_job_list function

--- src/insights.py
+++ src/insights.py
@@ -1,36 +1,47 @@
+from src.jobs import read
+
+
 def get_unique_job_types(path):
-    """Checks all different job types and returns a list of them
+    # """Checks all different job types and returns a list of them
 
-    Must call `read`
+    # Must call `read`
+    # read
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
+    # Parameters
+    # ----------
+    # path : str
+    #     Must be passed to `read`
 
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    return []
+    # Returns
+    # -------
+    # list
+    #     List of unique job types
+    # """
+    jobs_list = read(path)
+
+    unique_job_list = []
+    for job in jobs_list:
+        if job["job_type"] not in unique_job_list:
+            unique_job_list.append(job["job_type"])
+
+    return unique_job_list
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
+    # """Filters a list of jobs by job_type
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
+    # Parameters
+    # ----------
+    # jobs : list
+    #     List of jobs to be filtered
+    # job_type : str
+    #     Job type for the list filter
 
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
+    # Returns
+    # -------
+    # list
+    #     List of jobs with provided job_type
+    # """
     return []
 
 
