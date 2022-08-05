1e495d4888292b0025cfdf976fd7c97727f419cb
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-08-05 16:59:14 -0300

    test: add test_counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,6 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    assert count_ocurrences("src/jobs.csv", "Responsibilities") == 1645
+    assert count_ocurrences("src/jobs.csv", "xablau") == 0
