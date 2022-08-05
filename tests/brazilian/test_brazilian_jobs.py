811e9100f0c9b1c22232990d7ef66e3985725cca
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-08-05 19:17:08 -0300

    test: add test_brazilian_jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,15 @@
-# from src.brazilian_jobs import read_brazilian_file
+from unittest.mock import mock_open, patch
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    fake_data = "titulo,salario,tipo\nDeveloper,2000,effective"
+    expected_job_list = [
+        {"title": "Developer", "salary": "2000", "type": "effective"}
+    ]
+
+    with patch("builtins.open", mock_open(read_data=fake_data)):
+        assert (
+            read_brazilian_file("tests/mocks/brazilian_jobs.csv")
+            == expected_job_list
+        )
